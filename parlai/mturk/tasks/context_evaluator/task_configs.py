9d341ae3471b8f0d6ff5ba8a353d7d3804382a98
ethanjperez <ethanperez18@gmail.com>
2019-05-02 10:50:22 -0400

    Adjusting task

--- parlai/mturk/tasks/context_evaluator/task_configs.py
+++ parlai/mturk/tasks/context_evaluator/task_configs.py
@@ -73,7 +73,7 @@ task_configs = {
         """
     },
     'quote and question': {
-        'evaluation_data_dir': '../allennlp/eval/race/race.m=sl-sents.i.best.e/test',  # '../allennlp/eval/race/cross_ranker.last_epoch/test'
+        'evaluation_data_dir': '../allennlp/eval/race/race.m=sl-sents.best.e/test',  # '../allennlp/eval/race/cross_ranker.last_epoch/test'
         'reward': 1.5,  # RACE: 1.5, 11m, .75 bonus. DREAM: 1.12, 8m, .56 bonus
         'assignment_duration_in_seconds': 2700,
         'hit_description': 'Can you answer passage comprehension questions using just a quote?',
@@ -149,7 +149,7 @@ task_configs = {
     },
     'quotes and question': {
         # 'evaluation_data_dir': '../allennlp/eval/race/cross_ranker.best_epoch/test',
-        'evaluation_data_dir': '../allennlp/eval/dream/tfidf.o/test',
+        'evaluation_data_dir': '../allennlp/eval/dream/tfidf.o_q/test',
         'num_conversations': 25,
         'reward': 1.5,  # RACE: 2.0, 16m, 2.0 bonus. DREAM: 1.5, 11m, 1.5 bonus (or 1.2/1.2)
         'assignment_duration_in_seconds': 5400,
