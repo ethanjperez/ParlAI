4ac78bcac0c0ac622a1f19afcd3fdbfbae6ea95f
ethanjperez <ethanperez18@gmail.com>
2019-05-04 15:52:23 -0400

    Adjusting task

--- parlai/mturk/tasks/context_evaluator/task_configs.py
+++ parlai/mturk/tasks/context_evaluator/task_configs.py
@@ -148,7 +148,7 @@ task_configs = {
         """
     },
     'quotes and question': {
-        'evaluation_data_dir': '../allennlp/eval/race/race.m=sl.best.e/test',
+        'evaluation_data_dir': '../allennlp/eval/race/race.m=sl-sents.i.best.e/test',
         # 'evaluation_data_dir': '../allennlp/eval/dream/sl-sents-influence/test',
         'num_conversations': 25,
         'reward': 3.1,  # RACE: 3.1, 21-33m, 3.1 bonus. DREAM: 1.5, 11m, 1.5 bonus (or 1.2/1.2)
